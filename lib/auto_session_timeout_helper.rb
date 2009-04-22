744571e08c731a1fb2a196683ce329d2055931c6
Matthew Bass <matt@anacreon.local>
2009-04-22 17:59:23 -0400

    changed the timeout method to redirect to /logout

--- lib/auto_session_timeout_helper.rb
+++ lib/auto_session_timeout_helper.rb
@@ -4,7 +4,7 @@ module AutoSessionTimeoutHelper
 new Ajax.PeriodicalUpdater('', '/active', {frequency:60, method:'get', onSuccess: function(e) {
 	if ($('active_session').innerHTML == 'true' && e.responseText == 'false') {
 		alert('Your session has timed out.');
-		window.location.href = '/login';
+		window.location.href = '/logout';
 	}
 }});
 JS
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'test/unit'
 require 'action_controller'
-#require 'active_support/test_case'
 
 require File.expand_path(File.dirname(__FILE__) + '/../lib/auto_session_timeout')
