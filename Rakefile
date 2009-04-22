6a15373fc0e9855f28e37e45da6806f23e9dcdb7
Matthew Bass <matt@anacreon.local>
2009-04-22 10:15:43 -0400

    added JS helper

---
+++ Rakefile
@@ -0,0 +1,9 @@
+require 'rake'
+require 'rake/testtask'
+require 'rake/rdoctask'
+
+task :default => :test
+
+Rake::TestTask.new('test') do |t|
+  t.pattern = 'test/*_test.rb'
+end
---
+++ lib/auto_session_timeout_helper.rb
@@ -0,0 +1,17 @@
+module AutoSessionTimeoutHelper
+  def auto_session_timeout_js
+    code = <<JS
+new Ajax.PeriodicalUpdater('', '/active', {frequency:60, method:'get', onSuccess: function(e) {
+	if ($('active_session').innerHTML == 'true' && e.responseText == 'false') {
+		alert('Your session has timed out.');
+		window.location.href = '/login';
+	}
+}});
+JS
+    html = content_tag(:div, :id => "active_session") { logged_in? }
+    html << "\n" << javascript_tag(code)
+    html
+  end
+end
+
+ActionView::Base.send :include, AutoSessionTimeoutHelper
