aef964c95972b3823e605c215f8e33e0c7997b1b
Matthew Bass <matt@anacreon.local>
2009-04-22 10:38:26 -0400

    added action and updated readme

--- lib/auto_session_timeout.rb
+++ lib/auto_session_timeout.rb
@@ -1,7 +1,8 @@
 module AutoSessionTimeout
   
   def self.included(controller)
-    controller.extend(ClassMethods)
+    controller.extend ClassMethods
+    controller.hide_action :render_auto_session_timeout
   end
   
   module ClassMethods
@@ -16,6 +17,11 @@ module AutoSessionTimeout
         end
       end
     end
+  end
+  
+  def render_auto_session_timeout
+    response.headers["Etag"] = nil  # clear etags to prevent caching
+    render :text => logged_in?, :status => 200
   end
   
 end
