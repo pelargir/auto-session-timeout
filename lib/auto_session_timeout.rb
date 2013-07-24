a7ed0bb5957db77ed1e68cf7d1e11468f4129018
Matthew <pelargir@gmail.com>
2013-07-24 10:32:42 -0400

    gracefully handle current_user that does not respond to #auto_timeout

--- lib/auto_session_timeout.rb
+++ lib/auto_session_timeout.rb
@@ -12,7 +12,7 @@ module AutoSessionTimeout
           c.send :reset_session
         else
           unless c.url_for(c.params).start_with?(c.send(:active_url))
-            offset = seconds || current_user.try(:auto_timeout)
+            offset = seconds || (current_user.respond_to?(:auto_timeout) ? current_user.auto_timeout : nil)
             c.session[:auto_session_expires_at] = Time.now + offset if offset && offset > 0
           end
         end
