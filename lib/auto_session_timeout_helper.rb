172f9ae2fab61e8576fbb993dffa6db3697923a9
krishnasrihari <krishna.srihari@gmail.com>
2013-08-28 12:49:52 +0530

    jquery periodicalQuery

--- lib/auto_session_timeout_helper.rb
+++ lib/auto_session_timeout_helper.rb
@@ -6,6 +6,19 @@ if (typeof(Ajax) != 'undefined') {
   new Ajax.PeriodicalUpdater('', '/active', {frequency:#{frequency}, method:'get', onSuccess: function(e) {
     if (e.responseText == 'false') window.location.href = '/timeout';
   }});
+}else if(typeof(jQuery) != 'undefined'){
+  function PeriodicalQuery() {
+    $.ajax({
+      url: '/active',
+      success: function(data) {
+        if(data == 'false'){
+          window.location.href = '/timeout';
+        }
+      }
+    });
+    setTimeout(PeriodicalQuery, (frequency * 1000));
+  }
+  setTimeout(PeriodicalQuery, (frequency * 1000));
 } else {
   $.PeriodicalUpdater('/active', {minTimeout:#{frequency * 1000}, multiplier:0, method:'get', verbose:2}, function(remoteData, success) {
     if (success == 'success' && remoteData == 'false')
