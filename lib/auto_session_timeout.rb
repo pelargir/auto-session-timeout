083c89c5355c5b1d3f16aa547767a31778c72405
killernova <ye_sijie0101@wuxiapptec.com>
2017-06-08 14:57:18 +0800

    copy the session of csrf-toekn before reset session

--- lib/auto_session_timeout.rb
+++ lib/auto_session_timeout.rb
@@ -1,14 +1,16 @@
 module AutoSessionTimeout
-  
+
   def self.included(controller)
     controller.extend ClassMethods
   end
-  
+
   module ClassMethods
     def auto_session_timeout(seconds=nil)
       prepend_before_action do |c|
         if c.session[:auto_session_expires_at] && c.session[:auto_session_expires_at] < Time.now
+          old_session = session[:_csrf_token]
           c.send :reset_session
+          session[:_csrf_token] = old_session
         else
           unless c.request.original_url.start_with?(c.send(:active_url))
             offset = seconds || (current_user.respond_to?(:auto_timeout) ? current_user.auto_timeout : nil)
@@ -17,23 +19,23 @@ module AutoSessionTimeout
         end
       end
     end
-    
+
     def auto_session_timeout_actions
       define_method(:active) { render_session_status }
       define_method(:timeout) { render_session_timeout }
     end
   end
-  
+
   def render_session_status
     response.headers["Etag"] = ""  # clear etags to prevent caching
     render plain: !!current_user, status: 200
   end
-  
+
   def render_session_timeout
     flash[:notice] = "Your session has timed out."
     redirect_to "/login"
   end
-  
+
 end
 
 ActionController::Base.send :include, AutoSessionTimeout
